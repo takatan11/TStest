df5df93c2c5e5806ca7ac9a11f8a44cd0ffe02f6
takatan11 <ooame.100915@gmail.com>
2026-05-19 15:38:54 +0900

    JS勉強7日目

--- pra8.js
+++ pra8.js
@@ -1,11 +1,21 @@
 function toInitials(name) {
-   const words=name.split("");
-   for(const i=0;)
+   if(name===""){
+      return "";
+   }
+   const result=[];
+   const words=name.split(" ");
+ for(const n of words){
+   result.push(n[0].toUpperCase());
+ }
+ return result;
 }
 
-// 動作例
 console.log(toInitials("John Ronald Reuel Tolkien"));   // "J.R.R.T."
 console.log(toInitials("Alice"));                        // "A."
 console.log(toInitials("george orwell"));                // "G.O."
 console.log(toInitials("steve jobs"));                   // "S.J."
-console.log(toInitials(""));                             // ""
+                         // ""
+
+//入力された文字列空欄で分割して配列に保存。
+// 一つずつ取り出してばらばらにばらす。
+// それぞれの要素の最初の一文字を取り出して新しい配列に保存
